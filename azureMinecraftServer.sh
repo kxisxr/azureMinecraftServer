d06778647fc526d89f5c888c0eb8f4e0643fe5ae
kxisxr <47679627+k4is3r13@users.noreply.github.com>
2021-07-29 12:30:22 -0500

    Update azureMinecraftServer.sh

--- azureMinecraftServer.sh
+++ azureMinecraftServer.sh
@@ -49,7 +49,6 @@ echo -e "${greenColour}"'Installing openjdk 16... '"${endColour}"
 apt-get install openjdk-16-jdk --no-install-recommends -y >/dev/null 2>&1
 sleep 1
 
-clear
 echo -e "${greenColour}"'Installing misc... '"${endColour}"
 apt-get install unzip wget screen -y >/dev/null 2>&1
 sleep 1
