909cfe38f18866af012dc9c52325700b8dc7e2eb
kxisxr <47679627+k4is3r13@users.noreply.github.com>
2022-08-26 00:54:32 -0500

    Update azureMinecraftServer.sh

--- azureMinecraftServer.sh
+++ azureMinecraftServer.sh
@@ -43,6 +43,7 @@ echo -e "${turquoiseColour}"'[1] 1.17'"${endColour}"
 echo -e "${purpleColour}"'[2] 1.18 (experimental)'"${endColour}"
 echo -e "${yellowColour}"'[3] 1.18.1'"${endColour}\n"
 echo -e "${yellowColour}"'[4] 1.18.2'"${endColour}\n"
+echo -e "${yellowColour}"'[5] 1.19.2'"${endColour}\n"
 echo -e -n "${yellowColour}"'Version to install: '"${endColour}"
 read -e version
 echo -e ' '
@@ -88,6 +89,9 @@ wget https://launcher.mojang.com/v1/objects/125e5adf40c659fd3bce3e66e67a16bb49ec
 elif [ $version -eq 4 ]
 then
 wget https://launcher.mojang.com/v1/objects/c8f83c5655308435b3dcf03c06d9fe8740a77469/server.jar > /dev/null 2>&1
+elif [ $version -eq 5 ]
+then
+wget https://piston-data.mojang.com/v1/objects/f69c284232d7c7580bd89a5a4931c3581eae1378/server.jar
 else
 wget https://launcher.mojang.com/v1/objects/3cf24a8694aca6267883b17d934efacc5e44440d/server.jar > /dev/null 2>&1
 fi
